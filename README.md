227979121edda9278965f8c97579efb469e11dce
Souza <souzaneto25@github.com>
2023-04-07 16:35:02 -0300

    Atualizações finais no código

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# tracker
- Rastreador com GPS e ESP32
+# Tracker
+ Projeto de rastreador de geolocalização utilizando módulos GPS NEO-6M e o microcontrolador ESP32.
+
+#define RXD2 16                      // RXD2 do ESP32 conectado ao TX do GPS
+#define TXD2 17                      // TXD2 do ESP32 conectado ao RX do GPS
+#define GPS_BAUDRATE 9600            // Baud rate do GPS
+#define WIFI_SSID "PRUU"             // SSID da rede WiFi
+#define WIFI_PASSWORD "joaoetheo"    // Senha da rede WiFi
+#define MQTT_SERVER "broker.emqx.io" // Endereço do servidor MQTT
+#define MQTT_PORT 1883               // Porta do servidor MQTT
+#define MQTT_TOPIC "data/location"   // Tópico MQTT para publicar as coordenadas
+#define MQTT_USERNAME "emqx"         // Usuario do servidor
+#define MQTT_PASSWORD "public"       // Senha do usuario
+#define LED 2                        // Led interno do esp32
+#define LED_EXT 15                   // Led externo ao esp32
+#define DELAY 5000                   // Delay em ms para envio de dados. Ex:60*1000=5seg
+
+# 1. Rotas
+
+| CONSTANTES    | Descrição                                                                             |
+| ------------- | ------------------------------------------------------------------------------------- |
+| RXD2          | Pino RXD2 do ESP32. Obs: deve ser conectado ao TX do GPS                              |
+| TXD2          | Pino TXD2 do ESP32. Obs: deve ser conectado ao RX do GPS                              |
+| GPS_BAUDRATE  | Baud rate do GPS. Obs: Geralmente é de 9600                                           |
+| WIFI_SSID     | SSID da rede WiFi                                                                     |
+| WIFI_PASSWORD | Senha da rede WiFi                                                                    |
+| MQTT_SERVER   | Endereço do servidor MQTT                                                             |
+| MQTT_PORT     | Porta do servidor MQTT                                                                |
+| MQTT_TOPIC    | Tópico MQTT para publicar as coordenadas                                              |
+| MQTT_USERNAME | Usuario do servidor                                                                   |
+| MQTT_PASSWORD | Senha do usuario do servidor                                                          |
+| LED           | Pino referente ao LED interno do esp32                                                |
+| LED_EXT       | Pino referente ao LED externo ao esp32 que será utilizado. Obs: verificar o datasheet |
+| DELAY         | Delay em milisegundos (ms) para envio de dados. Ex: 5*1000 = 5 segundos               |
--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,30 @@
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
 
-#define RXD2 16                            // RXD2 do ESP32 conectado ao TX do GPS
-#define TXD2 17                            // TXD2 do ESP32 conectado ao RX do GPS
-#define GPS_BAUDRATE 9600                  // Baud rate do GPS
-#define WIFI_SSID "PRUU"                   // SSID da rede WiFi
-#define WIFI_PASSWORD "gustavoejulia"      // Senha da rede WiFi
-#define MQTT_SERVER "broker.emqx.io"       // Endereço do servidor MQTT
-#define MQTT_PORT 1883                     // Porta do servidor MQTT
-#define MQTT_TOPIC "esp32/gps/coordinates" // Tópico MQTT para publicar as coordenadas
-#define MQTT_USERNAME "emqx"               // Usuario do servidor
-#define MQTT_PASSWORD "public"             // Senha do usuario
-#define LED 2                              // Led interno do esp32
-#define LED_EXT 15                         // Led externo ao esp32
+#define RXD2 16                      // RXD2 do ESP32 conectado ao TX do GPS
+#define TXD2 17                      // TXD2 do ESP32 conectado ao RX do GPS
+#define GPS_BAUDRATE 9600            // Baud rate do GPS
+#define WIFI_SSID "PRUU"             // SSID da rede WiFi
+#define WIFI_PASSWORD "joaoetheo"    // Senha da rede WiFi
+#define MQTT_SERVER "broker.emqx.io" // Endereço do servidor MQTT
+#define MQTT_PORT 1883               // Porta do servidor MQTT
+#define MQTT_TOPIC "data/location"   // Tópico MQTT para publicar as coordenadas
+#define MQTT_USERNAME "emqx"         // Usuario do servidor
+#define MQTT_PASSWORD "public"       // Senha do usuario
+#define LED 2                        // Led interno do esp32
+#define LED_EXT 15                   // Led externo ao esp32
+#define DELAY 5000                   // Delay em ms para envio de dados. Ex:60*1000=5seg
 
-SoftwareSerial ss(RXD2, TXD2);        // Define a serial para comunicação com o GPS
-TinyGPSPlus gps;                      // Objeto para fazer o parsing dos dados do GPS
-WiFiClient wifiClient;                // Cliente WiFi
-PubSubClient client(wifiClient);      // Cliente MQTT
-const int json = JSON_OBJECT_SIZE(6); // Tamanho do objeto
-StaticJsonDocument<json> doc;         // Criação do documento (objeto que vai ser publicado)
+SoftwareSerial ss(RXD2, TXD2);         // Define a serial para comunicação com o GPS
+TinyGPSPlus gps;                       // Objeto para fazer o parsing dos dados do GPS
+WiFiClient wifiClient;                 // Cliente WiFi
+PubSubClient client(wifiClient);       // Cliente MQTT
+const int json = JSON_OBJECT_SIZE(10); // Tamanho do objeto
+StaticJsonDocument<json> doc;          // Criação do documento (objeto que vai ser publicado)
+size_t freeHeap = ESP.getFreeHeap();   // Quantidade de memória livre disponível na heap, que é a área de memória dinâmica usada pelo programa durante a execução
+
+DynamicJsonDocument doc2((freeHeap / 2)); // Criação de um documento com tamanho máximo igual a metade da memoria disponivel no ESP32
+JsonArray array = doc2.to<JsonArray>(); // Criação de um array de documentos
 
 // Função responsável por conectar ao Wifi
 void setup_wifi()
@@ -50,6 +55,17 @@ void setup_wifi()
   digitalWrite(LED, HIGH);
 }
 
+//Delay customizado com verificação do gps
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
+
 // Função de inicialização, após conectar ao wifi inicializa o servidor
 void setup()
 {
@@ -59,45 +75,88 @@ void setup()
   pinMode(LED_EXT, OUTPUT);
   setup_wifi();
   client.setServer(MQTT_SERVER, MQTT_PORT);
+  Serial.print(freeHeap);
 }
 
 void loop()
 {
-  // Lê os dados do GPS e aguarda até ter uma leitura válida
-  while (ss.available() > 0)
+  // Se os dados do GPS não estiverem disponíveis e válidos
+  if (millis() > 5000 && gps.charsProcessed() < 10)
   {
-    if (gps.encode(ss.read()))
-    {
-      break;
-    }
-  }
-
-  Serial.println("Satellites is valid " + String(gps.satellites.isValid()));
-  Serial.println("Values " + String(gps.satellites.value()));
-  Serial.println("{Lat: " + String(gps.location.lat(), 6) + "," + " Long: " + String(gps.location.lng(), 6) + "}");
-
-  // Se os dados do GPS estiverem disponíveis e válidos, envia as coordenadas para o servidor MQTT
-  if (gps.location.isValid())
-  {
-    String client_id = "esp32-client-";
-    client_id += String(WiFi.macAddress());
-    doc["device"] = "esp32";
-    doc["sensor"] = client_id;
-    doc["coordinates"]["lat"] = String(gps.location.lat(), 6);
-    doc["coordinates"]["lon"] = String(gps.location.lng(), 6);
-
-    char buffer[256];
-    size_t n = serializeJson(doc, buffer);
-
-    digitalWrite(LED_EXT, HIGH);
-    client.connect(client_id.c_str(), MQTT_USERNAME, MQTT_PASSWORD);
-    client.publish(MQTT_TOPIC, buffer, n);
-    client.disconnect();
-  }
-  else
-  {
+    Serial.println(F("No GPS data received: check wiring"));
     Serial.println("Esperando GPS...");
     digitalWrite(LED_EXT, LOW);
   }
-  delay(5000); // Aguarda 5 segundos antes de enviar a próxima mensagem
+  else
+  {
+
+    String client_id = "esp32-";
+    client_id += String(WiFi.macAddress());
+    String topic = String(MQTT_TOPIC) + "/" + client_id;
+    client.connect(client_id.c_str(), MQTT_USERNAME, MQTT_PASSWORD);
+
+    // se o cliente estiver conectado publica o
+    if (client.connected() && gps.location.isValid() && gps.hdop.isValid() && gps.satellites.isValid())
+    {
+      // se ja tiver dados armazenados, envia
+      String array_armazenado;
+      serializeJson(doc2, array_armazenado);
+      if (array_armazenado.length() > 2)
+      {
+        Serial.println("_______________Old array location_________________");
+        Serial.print("array_armazenado length: ");
+        Serial.println(array_armazenado);
+        client.publish(topic.c_str(), array_armazenado.c_str());
+        array = doc2.to<JsonArray>();
+        Serial.println("__________________________________________________");
+      }
+      Serial.println("...................New location....................");
+      String new_location;
+      doc.clear();
+      doc["device"] = "esp32";
+      doc["sensor"] = client_id;
+      doc["coordinates"]["lat"] = String(gps.location.lat(), 6);
+      doc["coordinates"]["lon"] = String(gps.location.lng(), 6);
+      serializeJson(doc, new_location);
+      digitalWrite(LED_EXT, HIGH);
+
+      client.publish(topic.c_str(), new_location.c_str());
+
+      delay(1000);
+      digitalWrite(LED_EXT, LOW);
+      Serial.println("Topic " + topic);
+      Serial.println("Sats " + String(gps.satellites.value()));
+      Serial.println("HDOP " + String(gps.hdop.hdop()));
+      Serial.println(new_location);
+      Serial.println("...................................................");
+
+      client.disconnect();
+    }
+    else
+    {
+      Serial.println("...............Client disconnected.................");
+      if (gps.location.isValid() && gps.hdop.isValid() && gps.satellites.isValid())
+      {
+        Serial.println("_______________New location saved_________________");
+        JsonObject obj = array.createNestedObject();
+        obj["device"] = "esp32";
+        obj["sensor"] = client_id;
+        obj["coordinates"]["lat"] = String(gps.location.lat(), 6);
+        obj["coordinates"]["lon"] = String(gps.location.lng(), 6);
+        Serial.println("{device:esp32, sensor:" + client_id + ", lat:" + String(gps.location.lat(), 6) + ", lon:" + String(gps.location.lng(), 6) + "}");
+        String output;
+        serializeJson(doc2, output);
+        Serial.print("Output length: ");
+        Serial.println(output.length());
+        Serial.println("__________________________________________________");
+      }
+      else
+      {
+        Serial.println("..................Searching sats...................");
+        Serial.println("{device:esp32, sensor:" + client_id + ", lat:" + String(gps.location.lat(), 6) + ", lon:" + String(gps.location.lng(), 6) + "}");
+      }
+      Serial.println("...................................................");
+    }
+  }
+  smartDelay(DELAY);
 }
